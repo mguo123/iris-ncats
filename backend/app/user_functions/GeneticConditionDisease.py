6364beb32a3c48a520dc6124c8154d4ece3dafd1
mguo123 <mguo123@stanford.edu>
2017-11-26 22:46:08 -0800

    added in scripts from Q1 (greg)

--- backend/app/user_functions/GeneticConditionDisease.py
+++ backend/app/user_functions/GeneticConditionDisease.py
@@ -1,29 +1,57 @@
 from iris import state_types as t
 from iris import IrisCommand
 from app.user_functions.Q1_main import Q1_query
-
+from iris import iris_objects
+import os
 
 class GeneticConditionDisease(IrisCommand):
     # what iris will call the command + how it will appear in a hint
     # title = "how does {condition} protects against {condition}?"
     title = "What genetic disease protects against {condition}?"
     # give an example for iris to recognize the command
-    examples = ["what protects against {condition}", "what genetic conditions might offer protection against {condition} and why", "protective mechanism of condition against disease", "protective condition", "protection against disease"]
+    examples = ["what protects against {condition}",
+                "what genetic conditions might offer protection against {condition} and why",
+                "protective mechanism of condition against disease",
+                "protective condition",
+                "protection against disease",
+                "do any genetic diseases protect against {condition}"]
     # type annotations for each command argument, to help Iris collect missing values from a user
     argument_types = {"condition":t.String("Just for confirmation: What is the condition do you want to analyze?")} 
                         # ,"genetic_disease":t.String("What is the genetic disease do you think it might link to? If unknown, type none")}
     
     # core logic of the command
     def command(self, condition):
-        # import numpy as np
-        return Q1_query(condition)
+        # Run the query
+        results = Q1_query(condition)
+
+        return results
 
     # wrap the output of a command to display to user
     # by default this will be an identity function
     # each element of the list defines a separate chat bubble
     def explanation(self, result):
+        """"# results is an object with       # Pandas data frame with top 10 resutls
+        self.similarities = None
+        # List of paths to word clouds
+        self.commonality_clouds = []
+        """
+        result_array = []
+        if result.error is not None:
+            result_array.append('There was an error processing your request')
+            return result_array
 
-        return result
+        result_array.append('The following genetic diseases are the most semantically similar to your query')
+        # adds the table to results
+        similarities_df = iris_objects.IrisDataframe(data=result.top_similarities())
+        self.iris.add_to_env('similarities', similarities_df)
+        result_array.append(similarities_df)
+
+        # display image (first one)
+        #if len(result.commonality_clouds > 0):
+        #    os.system("open " + result.commonality_clouds[0])
+
+
+        return result_array
 
 
 _GeneticConditionDisease = GeneticConditionDisease()
---
+++ backend/app/user_functions/ncats/scripts/Q1/search_cooccurence.R
@@ -0,0 +1,165 @@
+# Provided with two disease names, 
+# 1. search pubmed for coccurences
+# 2. Compute tf-idf using the individual diseases as background
+# 3. Generate word clouds for the tf-idf weighted terms
+
+library(PubMedWordcloud)
+library(optparse)
+library(lsa)
+library(dplyr)
+library(tidytext)
+library(wordcloud)
+
+option_list = list(
+  make_option(c("-a", "--disease_1"), type="character", default=NULL, 
+              help="Disease name", metavar="character"),
+  make_option(c("-b", "--disease_2"), type="character", default=NULL, 
+              help="Second disease name", metavar="character"),
+  make_option(c("-d", "--data_dir"), type="character", default=NULL),
+  make_option(c("-p", "--prefix"), type="character", default="cooccurence", 
+              help="Output prefix", metavar="character"))
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+data_dir = opt$data_dir
+
+
+disease_1 = opt$disease_1
+disease_2 = opt$disease_2
+
+
+
+clean_query <- function(query) {
+  clean_query <- tolower(query)
+  clean_query <- gsub(" ", "_", clean_query, fixed=TRUE)
+  clean_query <- gsub(",", "", clean_query, fixed=TRUE)
+  return(clean_query)
+}
+
+
+# Convert abstracts into tidy objects
+tidy_up_text <- function(a) {
+  text_df <- data_frame(line=1:length(a), text = a)
+  
+  tidy_terms<- text_df %>%
+    unnest_tokens(word, text)
+  
+  data(stop_words)
+  
+  tidy_terms <- tidy_terms %>%
+    anti_join(stop_words)
+  
+  return(tidy_terms)
+}
+
+# Function to format abstracts downloaded from pubmed
+format_abstracts <- function(abstracts, disease) {
+  query_terms <- tidy_up_text(abstracts)
+  query_terms$disease = disease
+  
+  # Get counts for each word by disease
+  query_disease_terms <- query_terms %>%
+    count(disease, word, sort = TRUE) %>%
+    ungroup()
+  
+  # Get sums for each disease
+  total_words <- query_disease_terms %>% 
+    group_by(disease) %>% 
+    summarize(total = sum(n))
+  
+  # Add the total count to the disease words
+  query_disease_terms <- left_join(query_disease_terms, total_words)
+  
+  return(query_disease_terms)
+}
+
+fetch_abstracts <- function(disease_name) {
+  query_abstracts = NA
+  clean_q <- clean_query(disease_name)
+  query_abstract_filename <- paste("abstracts", clean_q, "txt", sep=".")
+  query_abstract_file_path <- paste(data_dir, query_abstract_filename, sep="/")
+  if (file.exists(query_abstract_file_path)) {
+    # Read in the abstracts
+    print("Found abstracts locally")
+    #print(query_abstract_file_path)
+    query_abstracts <- read.table(query_abstract_file_path, header=T, stringsAsFactors=F, sep="\t", quote=NULL)
+    #print(head(query_abstracts))
+    query_abstracts <- query_abstracts[['ABSTRACT']]
+    #print(head(query_abstracts))
+  } else {
+    # 3b. If not, download them from PubChem  
+    print("Downloading abstracts")
+    query_abstracts <- getAbstracts(getPMIDsByKeyWords(keys = disease_name, dFrom = 2000, dTo = 2016)[1:1000])
+    query_abstracts <- unlist(query_abstracts)
+    # 3ba. Save the abstracts to the data directory
+    abstracts_df <- data.frame(query_abstracts)
+    print(head(abstracts_df))
+    names(abstracts_df) <- "ABSTRACT"
+    abstracts_df <- data.frame(lapply(abstracts_df, function(x) {
+      gsub("#", "", x)
+    }))
+    write.table(abstracts_df, file=query_abstract_file_path, quote=F, row.names = F) 
+  }
+  return(query_abstracts)
+}
+
+fetch_terms <- function(disease_name) {
+  clean_q <- clean_query(disease_name)
+  query_abstracts = fetch_abstracts(disease_name)
+  # Check if there are no abstracts or if there are too few
+  if (length(query_abstracts) < 10) {
+    # Should do something to tell python that it broke.  Maybe save a run diagnostics object and load it with ryp2. 
+    print(query_abstracts)
+    quit()
+  }
+  
+  # 3c. Process the abstracts and append them to the genetic disease terms
+  query_disease_terms <- format_abstracts(query_abstracts, clean_q)
+  return(query_disease_terms)
+  #disease_terms <- rbind(query_disease_terms, disease_terms)
+}
+
+
+disease_1_terms <- fetch_terms(disease_1)
+disease_2_terms <- fetch_terms(disease_2)
+both_terms <- fetch_terms(paste(disease_1, disease_2))
+
+disease_terms <- rbind(disease_1_terms, disease_2_terms)
+disease_terms <- rbind(disease_terms, both_terms)
+
+# 4. Calculate tf-idf on the disease terms
+disease_terms <- disease_terms %>%
+  bind_tf_idf(word, disease, n)
+disease_terms
+
+disease_terms[with(disease_terms, order(-tf_idf)), ]
+
+both_clean <- clean_query(paste(disease_1, disease_2))
+both <- disease_terms[disease_terms$disease == both_clean,]
+
+tfidf_output_file <- paste(opt$prefix, clean_query(disease_1), clean_query(disease_2), "cooccurence_tfidf.png", sep=".")
+freq_output_file <- paste(opt$prefix, clean_query(disease_1), clean_query(disease_2), "cooccurence_frequency.png", sep=".")
+
+png(tfidf_output_file, width=12, height=8, units="in", res=300)
+wordcloud(word=both$word, freq=both$tf_idf, max.words = 100, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+dev.off()
+
+png(freq_output_file, width=12, height=8, units="in", res=300)
+wordcloud(word=both$word, freq=both$n, max.words = 100, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+dev.off()
+
+
+# Search for word combos
+both_abstracts <- fetch_abstracts(paste(disease_1, disease_2))
+text_df <- data_frame(line=1:length(both_abstracts), text = both_abstracts)
+tidy_abstract_sentences <- text_df %>%
+  unnest_tokens(word, text, token="sentences")
+
+
+d1 <- tidy_abstract_sentences[grepl(disease_1,tidy_abstract_sentences$word, perl=T, ignore.case = T), ]
+d2 <- d1[grepl(disease_2,d1$word, perl=T, ignore.case = T), ]
+
+# Sentences with both -SAVE
+sentence_output_file <- paste(opt$prefix, clean_query(disease_1), clean_query(disease_2), "cooccurence_sentences.txt", sep=".")
+write.table(d2, file=sentence_output_file, sep="\t", row.names = F)
