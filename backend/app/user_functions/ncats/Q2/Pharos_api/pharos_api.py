5e9978baa10acddb6990db24b547021c642c8e59
alavertu <adamlavertu@gmail.com>
2017-11-27 20:57:37 -0800

    commented code

--- backend/app/user_functions/ncats/Q2/Pharos_api/pharos_api.py
+++ backend/app/user_functions/ncats/Q2/Pharos_api/pharos_api.py
@@ -9,15 +9,9 @@ PATH_TO_GIANT_TISSUES_LIST=os.path.join(RSCS_DIR, 'list_of_GIANT_tissues.txt')
 
 def query(data_type, uid):
     """
-    query
-
-    Generates a Pharos API query and returns the results
-
-    Input:
-    data_type - the Pharos data type (e.g. ligand, target ..)
-    uid - Pharos concept identifier
-    Output:
-    JSON containing the query results
+    :param data_type: the Pharos data type (e.g. ligand, target ..)
+    :param uid: Pharos concept identifier
+    :return: JSON containing the query results
     """
     url = "https://pharos.nih.gov/idg/api/v1/{}({})?view=full".format(data_type, uid)
     try:
@@ -34,18 +28,8 @@ def query(data_type, uid):
 ####################################################################################################
 def get_disease_id(disease_common_name):
     """
-    get_disease_id
-
-    Queries Pharon API and return the Pharon ID for the disease if it is in Pharos
-
-    Input: 
-    disease_common_name - name of the disease query, will search for this term
-
-    output:
-    disease_id = Pharoid uid for the queried disease, if none exists, return None
-
-
-
+    :param disease_common_name: name of the disease query, will search for this term
+    :return:Pharos uid for the queried disease, if none exists, return None
     """
     disease_common_name = disease_common_name.replace(" ", "%20").lower()
     p = nltk.PorterStemmer()
@@ -60,6 +44,10 @@ def get_disease_id(disease_common_name):
     return disease_id
 
 def disease_synonym_data(qID):
+    """
+    :param qID: The Pharos ID for the disease
+    :return: response in a JSON format
+    """
     url = "https://pharos.nih.gov/idg/api/v1/diseases/{}/synonyms".format(qID)
     info = requests.get(url).json()
     return info
@@ -72,15 +60,8 @@ def disease_synonym_data(qID):
 
 def get_ligand_id(ligand_common_name):
     """
-    get_ligand_id
-
-    Queries the Pharos API and returns the Pharos ID for the ligand if it is in Pharos
-
-    Input:
-    ligand_common_name - the common name for the drug, this will be the search term for Pharos
-
-    Output:
-    ligand_id - Pharos uid for the queried drug, if one exists, else None is returned
+    :param ligand_common_name: the common name for the drug, this will be the search term for Pharos
+    :return: Pharos uid for the queried drug, if one exists, else None is returned
     """
     url = "https://pharos.nih.gov/idg/api/v1/ligands/search?q={}&top=10".format(ligand_common_name)
 
@@ -94,14 +75,8 @@ def get_ligand_id(ligand_common_name):
 
 def get_ligand_targets(ligand):
     """
-    get_ligand_targets
-
-    Queries the Pharos API and returns the drug targets for the ligand provided
-    Input:
-    ligand - Pharos uid for the drug
-
-    Output:
-    putative_targets - a list of putative targets, return None if there were no known targets in Pharos
+    :param ligand: Pharos uid for the drug
+    :return: a list of putative targets, return None if there were no known targets in Pharos
     """
     qID = get_ligand_id(ligand)
     resp = query("ligands", qID)
@@ -125,13 +100,8 @@ def get_ligand_targets(ligand):
 ####################################################################################################
 def get_target_id(target_common_name):
     """
-    get_target_id(target_common_name)
-
-    Input: 
-    target_common_name - drug name
-
-    Output: 
-    target_id - corresponding Pharos ID for the target
+    :param target_common_name: target name (NCBI gene name)
+    :return: corresponding Pharos ID for the target
     """
     try:
         resp = query("targets", target_common_name)
@@ -144,32 +114,21 @@ def get_target_id(target_common_name):
 
 
 def target_prop_data(qID, label):
-
     """
-    target_prop_data(qID, label)
-
-    Input: 
-    qID - Pharos ID for the gene target
-    label - property of interest
-
-    Output: 
-    info - list of dictionaries containing the properties data with the specified label
+    :param qID: Pharos ID for the gene target
+    :param label: property of interest
+    :return: info - JSON of the query results
     """
+
     url = "https://pharos.nih.gov/idg/api/v1/targets/{}/properties(label={})".format(qID, label)
     info = requests.get(url).json()
     return info
 
 def target_link_data(qID, kind):
-
     """
-    target_link_data(qID, label)
-
-    Input: 
-    qID - Pharos ID for the gene target
-    label - link of interest
-
-    Output: 
-    info - list of dictionaries containing the link data with the specified label
+    :param qID: Pharos ID for the gene target
+    :param kind: link of interest
+    :return:
     """
     url = "https://pharos.nih.gov/idg/api/v1/targets/{}/links(kind={})".format(qID, kind)
     info = requests.get(url).json()
@@ -190,13 +149,8 @@ def get_target_interactions(qID):
 
 def get_pathways(target_name):
     """
-    get_pathways(target_name)
-
-    Input: 
-    targets - drug name
-
-    Output: 
-    pathways, list of pathways relevant for disease (use KEGG Pathway, Reactome Pathway, PathwayCommons*, WikiPathways Pathway)
+    :param target_name: NCBI gene name
+    :return: pathways, list of pathways relevant for disease (use KEGG Pathway, Reactome Pathway, PathwayCommons*, WikiPathways Pathway)
     """
     qID = get_target_id(target_name)
 
@@ -215,14 +169,10 @@ def get_pathways(target_name):
 
 def get_GO_terms(target_name):
     """
-    get_GO_terms(target_name)
-
-    Input: 
-    targets - drug name
-
-    Output: 
-    GO_terms - list of GO process, function, and component (list of 3 dictionaries) terms for that drug
+    :param target_name: NCBI gene name
+    :return: list of GO process, function, and component (list of 3 dictionaries) terms for that drug
     """
+
     # Query for the Pharos ID of the Gene target
     qID = get_target_id(target_name)
 
@@ -252,14 +202,10 @@ def get_GO_terms(target_name):
 
 def get_target_tissue(target_name):
     """
-    get_target_tissue(target_name)
-
-    Input: 
-    targets - drug name
-
-    Output: 
-    tissues - list of tissues for that drug
+    :param target_name: NCBI gene name
+    :return: set of the UNIPROT tissue annotations for that target
     """
+
     # Query for the Pharos ID of the Gene target
     qID = get_target_id(target_name)
     if qID is None:
@@ -280,14 +226,10 @@ def get_target_tissue(target_name):
 
 def get_tissues_oi(targets):
     """
-    get_tissues_oi(targets)
-
-    Input: 
-    targets - list of gene targets
-
-    Output: 
-    tissues_oi - list of tissues where those genes are expressed
+    :param targets: list of gene targets (NCBI IDs)
+    :return: dictionary of tissues where those genes are expressed and the count of genes expressed in those tissues
     """
+
     tissues_oi = dict()
     # Get tissues associated with each target and reformat for compatibility with GIANT
     for target in targets:
@@ -307,6 +249,9 @@ Input: None
 Output: Creates a global environment variable that contains the list of acceptable GIANT tissues
 """
 def init_GIANT_queries():
+    """
+    :return: Creates a global environment variable that contains the list of acceptable GIANT tissues
+    """
     global GIANT_tissues
     GIANT_tissues = set()
     with open(PATH_TO_GIANT_TISSUES_LIST) as inFile:
@@ -323,6 +268,10 @@ Output:
 List of GIANT tissues that overlap with the tissues of interest
 """
 def get_GIANT_tissues_oi(tissues):
+    """
+    :param tissues: iterable set of tissues of interest, as formatted by get_tissues_oi
+    :return: List of GIANT tissues that overlap with the tissues of interest
+    """
     GIANT_tissues_oi = set()
     for tissue in tissues.keys():
         if tissue in GIANT_tissues:
@@ -338,35 +287,24 @@ Output:
 dictionary of tissues with the count of the number of 
 """
 def get_drug_tissues(drug_name):
+    """
+    :param drug_name: common drug name all lowercase
+    :return: dictionary of tissues with the count of the number of times a gene was annotated to each tissue
+    """
     ligand_targets = get_ligand_targets(drug_name)
     target_tissues = get_tissues_oi(ligand_targets)
     return(target_tissues)
 
 
-# if __name__ == "__main__":
-#     # get_drug_tissues("adapalene")
-#     ligand_targets = get_ligand_targets("adapalene")
-#     print(ligand_targets)
-    # print(get_disease_id("acne vulgaris"))
-    # qID = get_target_id(ligand_targets[0])
-    # print(qID)
-
-    # print(get_GO_terms('RARG'))
-    # print(get_target_id('RARG'))
-    # print(get_pathways('RARG'))
-    # print(get_drug_tissues("adapalene"))
-    # print(get_tissues_oi(["RARA"]))
-    # temp = get_target_interactions(qID)
-    # print("GENE TARGETS:", ligand_targets)
-    # target_tissues = get_tissues_oi(ligand_targets)
-    # print("GENE TISSUES:", target_tissues)
-    # init_GIANT_queries()
-    # GIANT_tiss = get_GIANT_tissues_oi(target_tissues)
-    # print("GIANT TISSUES:", GIANT_tiss)
-
-
-    """
-    Things i want for iris:
-    get_drug_tissues - takes in a common drug name, outputs a dictionary with tissue-count key value pairs
-    get_tissues_oi - takes in a list of gene names, outputs a dictionary with tissue-count key value pairs
-    """
+if __name__ == "__main__":
+    ligand_targets = get_ligand_targets("adapalene")
+    print(ligand_targets)
+    print(get_disease_id("acne vulgaris"))
+    qID = get_target_id(ligand_targets[0])
+    temp = get_target_interactions(qID)
+    print("GENE TARGETS:", ligand_targets)
+    target_tissues = get_tissues_oi(ligand_targets)
+    print("GENE TISSUES:", target_tissues)
+    init_GIANT_queries()
+    GIANT_tiss = get_GIANT_tissues_oi(target_tissues)
+    print("GIANT TISSUES:", GIANT_tiss)
